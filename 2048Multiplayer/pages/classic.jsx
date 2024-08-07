e6117d4af6d47a05c13ade2e983c9b5d2356d744
Ravid-A <ravid2903@gmail.com>
2024-08-07 00:27:35 +0000

    Added checks for user for all gamemodes

--- 2048Multiplayer/pages/classic.jsx
+++ 2048Multiplayer/pages/classic.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import OfflineGame from "../components/Game/OfflineGame";
+
+import GetUser from '../utilities/GetUser'
 
 import Game from "../states/GameObserver";
 
 export default function Classic() {
+  const router = useRouter();
   const game = new Game("classic");
+
+  const getUser = async () => {
+    const user_data = await GetUser();
+
+    if (!user_data) {
+      router.push("/");
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return <OfflineGame game={game} />;
 }
--- 2048Multiplayer/pages/speedrun.jsx
+++ 2048Multiplayer/pages/speedrun.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import SpeedrunGame from "../components/Game/SpeedrunGame";
+
+import GetUser from '../utilities/GetUser'
 
 import Game from "../states/GameObserver";
 
 export default function Speedrun() {
+  const router = useRouter();
   const game = new Game("speedrun");
+
+  const getUser = async () => {
+    const user_data = await GetUser();
+
+    if (!user_data) {
+      router.push("/");
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return <SpeedrunGame game={game} />;
 }
